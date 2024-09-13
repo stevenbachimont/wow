58ce62893852170492378e21e174c6a4ea832b23
stevenbachimont <stevenbachimont@gmail.com>
2024-09-13 08:23:38 +0200

    ajout

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    host: true,
+    port: 8181,
+    watch: {
+      usePolling: true
+    }
+  }
 })
